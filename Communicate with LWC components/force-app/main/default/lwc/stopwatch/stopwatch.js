8dd0ab80877001269e38fe0acfeac3f6060f68e1
Wajid Ali <wajid.ali@ocloudsolutions.net>
2023-04-06 12:22:21 +0500

    Create StopWatch UI and Functionality

--- Communicate with LWC components/force-app/main/default/lwc/stopwatch/stopwatch.js
+++ Communicate with LWC components/force-app/main/default/lwc/stopwatch/stopwatch.js
@@ -1,3 +1,37 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Stopwatch extends LightningElement {}
+export default class Stopwatch extends LightningElement {
+    @track time = 0;
+  @track isRunning = false;
+  @track isNotRunning = false;
+  interval;
+
+  get timeString() {
+    const minutes = Math.floor(this.time / 60);
+    const seconds = this.time % 60;
+    return `${minutes.toString().padStart(2, '0')}:${seconds
+      .toString()
+      .padStart(2, '0')}`;
+  }
+
+  handleStart() {
+    this.isRunning = true;
+    this.isNotRunning = false;
+    this.interval = setInterval(() => {
+      this.time += 1;
+    }, 1000);
+  }
+
+  handlePause() {
+    this.isRunning = false;
+    this.isNotRunning = true;
+    clearInterval(this.interval);
+  }
+
+  handleReset() {
+    this.time = 0;
+    this.isRunning = false;
+    this.isNotRunning = true;
+    clearInterval(this.interval);
+  }
+}
