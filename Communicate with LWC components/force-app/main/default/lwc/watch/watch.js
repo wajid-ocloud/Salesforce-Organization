a4282bb6d97440c2d5ce79c87d56ec42cdf23db1
Wajid Ali <wajid.ali@ocloudsolutions.net>
2023-04-06 20:07:26 +0500

    Create Timer UI

--- Communicate with LWC components/force-app/main/default/lwc/watch/watch.js
+++ Communicate with LWC components/force-app/main/default/lwc/watch/watch.js
@@ -1,3 +1,30 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 
-export default class Watch extends LightningElement {}
+export default class Watch extends LightningElement {
+
+    @track isDisplayTimer = true;
+    showFeatures = true;
+    timeString = '00h00m00s';
+    isRunning = false;
+    hour1 = 0;
+    hour2 = 0;
+    min1 = 0;
+    min2 = 0;
+    sec1 = 0;
+	inputDigits = [];
+    handleShowTimer(event){
+        this.isDisplayTimer = true;
+    }
+    handleShowInput(event){
+        this.isDisplayTimer = false;
+    }
+
+    handleKeyDown(event) {
+    const keyCode = event.keyCode || event.which;
+    if (keyCode >= 48 && keyCode <= 57) {
+      console.log(`Number pressed: ${event.key}`);
+
+    }
+  }
+
+}
